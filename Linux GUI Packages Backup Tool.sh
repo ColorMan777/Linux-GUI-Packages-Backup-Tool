619c3f8467d23533d415bf8cb7415fd6235e3a93
ColorMan777 <97909940+ColorMan777@users.noreply.github.com>
2022-07-04 19:53:36 +0000

    Update Linux GUI Packages Backup Tool.sh

--- Linux GUI Packages Backup Tool.sh
+++ Linux GUI Packages Backup Tool.sh
@@ -33,14 +33,14 @@ while true; do
         case $? in
             0)
                 echo "\"$LOAD_PATH\" is selected."
-                function install_ignore_fail { echo "$*" | xargs -n 1 sudo apt-get install -y; }
+                function install_ignore_fail { echo "$*" | xargs -n 1 sudo -S apt-get install -y; }
 
-                 while ! zenity --password | sudo -S install_ignore_fail $(cat $LOAD_PATH); do # Actual Action
+                 while ! zenity --password | install_ignore_fail $(cat $LOAD_PATH); do # Actual Action
                     if $(zenity --question --text="Wrong password, would you like to cancel the installation?"); then
                     echo "no app-entry made, returning"
                     return;
                     fi
-                done
+                done;;
                     #zenity --password | sudo apt-get install $(cat $LOAD_PATH);; # Actual Action
             1)
                 echo "No files selected.";;
